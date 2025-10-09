e1fdb84216bb1373cd77f826d4abc56553b93f41
DoughnutDev <gunnarmstinson@gmail.com>
2025-10-09 16:42:59 -0400

    Fix: Disable default help command to use custom one

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 # Global variables
 active_spawns = {}  # {channel_id: {pokemon_data, spawn_time}}
